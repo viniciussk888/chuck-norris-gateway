1318d8820f37b061398d797a866bcb255fe73f34
Vinicius Martins <vinicius.cross07@gmail.com>
2024-06-20 06:01:04 -0300

    feature: implementa gateway chuck api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import {WinstonModule} from "nest-winston";
 import {ConfigModule} from "@nestjs/config";
 import winstonConfig from "./common/winston.logger";
 import {HealthcheckModule} from "./modules/healthcheck/healthcheck.module";
+import {ChuckNorrisModule} from "./modules/chuck-norris/chuck-norris.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     HealthcheckModule,
+    ChuckNorrisModule,
     WinstonModule.forRoot(winstonConfig)
   ]
 })
